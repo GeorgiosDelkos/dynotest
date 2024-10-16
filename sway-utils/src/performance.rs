1cac212a8cdc8ad9793b3801e17557f5702bf4e7
GeorgiosDelkos <georgios.delkos@ourovoros.io>
2024-10-16 11:08:45 +0300

    Refactor performance.rs to improve code efficiency

--- sway-utils/src/performance.rs
+++ sway-utils/src/performance.rs
@@ -43,6 +43,8 @@ macro_rules! time_expr {
                     x.push(a);
                     let b = x * a;
                     x.push(b);
+                    let c = x * b;
+                    x.push(c);
                 }
             }
             let output = { $expression };
